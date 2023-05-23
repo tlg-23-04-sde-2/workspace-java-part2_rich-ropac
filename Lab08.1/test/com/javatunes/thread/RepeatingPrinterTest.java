b9ee8f4d0db5bc8475d67ed283089f3e33e4caa6
Richard Ropac <Richard.Ropac@tlgcohort.com>
2023-05-23 12:32:22 -0500

    Lab08.1 Complete

--- Lab08.1/test/com/javatunes/thread/RepeatingPrinterTest.java
+++ Lab08.1/test/com/javatunes/thread/RepeatingPrinterTest.java
@@ -10,15 +10,15 @@ package com.javatunes.thread;
 
 public class RepeatingPrinterTest {
 
-  public static void main(String[] args) {
-    Thread thd1 = new Thread(new RepeatingPrinter());
-    thd1.start();
-    
-    // TODO: initialize the 'printer' reference variable with a block lambda
-    // hint: you can copy / paste the *contents* of RepeatingPrinter's run() method
-    // note: change the sysout to show "block-lambda"
-    Runnable printer = null;
-    
-    // TODO: create another Thread, passing in the 'printer' reference as its Runnable - then start it
-  }
+    public static void main(String[] args) {
+        Thread thd1 = new Thread(new RepeatingPrinter());
+        thd1.start();
+
+        // TODO: initialize the 'printer' reference variable with a block lambda
+        // hint: you can copy / paste the *contents* of RepeatingPrinter's run() method
+        // note: change the sysout to show "block-lambda"
+        Runnable printer = null;
+
+        // TODO: create another Thread, passing in the 'printer' reference as its Runnable - then start it
+    }
 }
